872d7fe65d480d9de89ff227135046af1e0b9858
riverofrancisco <francisco.rivero124@gmail.com>
2024-07-04 19:38:38 +0200

    buildTOdist and ignore

--- api/build/routes/meli.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const meliProducts_1 = require("./mercadopago/meliProducts");
-const succesPay_1 = require("./mercadopago/succesPay");
-const router = (0, express_1.Router)();
-router.post("/", meliProducts_1.payMeli);
-router.post("/email", succesPay_1.successMail);
-module.exports = router;
